91971b8c02ebba0e0280c5ba2ee717a543ce1a2c
Ajay Kumar <yajaykumar111@gmail.com>
2020-11-25 10:48:35 +0530

    Project Completed

--- languagesproject/settings.py
+++ languagesproject/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'f+@w(jx1dxoh0j09!=$dyirlkt5x$@o_=oiw(yzar=97o04@2%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
